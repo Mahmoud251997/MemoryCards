41e11bf2904ae2b1a90e28b070dc54d928c5e8ee
mahmoud Diab <mhmoddiab2@gmail.com>
2021-01-08 19:21:35 +0200

    add sounds

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,4 @@
+
 const cards = document.querySelectorAll('.memory-card');
 
 
@@ -5,22 +6,59 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+startGameS = document.createElement("audio");
+startGameS.src = "sounds/StartGame.mp3";
+flipSound = document.createElement("audio");
+flipSound.src = "sounds/filpCard.wav";
+gameOver = document.createElement("audio");
+gameOver.src = "sounds/GameEnd.mp3";
+victoryS = document.createElement("audio");
+victoryS.src = "sounds/victory.mp3";
+wrongAs = document.createElement("audio");
+wrongAs.src = "sounds/WrongAnswerSound.wav";
+matchSound = document.createElement("audio");
+matchSound.src = "sounds/match.wav";
 
-   function flipCard() {
 
+let overlays = Array.from(document.getElementsByClassName('overlay-text')); // make array from class name overlay-text witch have visible word that we will be change it
+
+overlays.forEach(overlay => {   // make click event to remove visisble word to disapper and start game and add audio when we start the game
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('visible');
+        //startSound.play(); 
+        startGameS.play();
+    });  
+
+});
+
+
+
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+   function flipCard() {   
+    
+    
       if (lockBoard) return; // return from the function if lock board is true so the rest wonn't executed.
       if (this === firstCard) return; // when you click twice on the same card it will return the function.
 
        this.classList.add('flip');
-      if (!hasFlippedCard) {  //the played clicked first card
+      if (!hasFlippedCard) { //the played clicked first card
+        
       hasFlippedCard = true;
       firstCard = this;
+      flipSound.play();
 
       return;
       } 
      //second click
    
-     secondCard = this;  
+     secondCard = this 
+    
 
       checkForMatch();
    }  
@@ -33,29 +71,35 @@ function checkForMatch() {
       
 }
 function disableCards() {
+
+    flipSound.play();
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-
-    resetBoard()
+    matchSound.play();
+    
+    
+    resetBoard();
 }
 
 function unflipCards() {
-    lockBoard = true; // unlocked when the cards finish on flipping
-
+    flipSound.play();
+    lockBoard = true;  // unlocked when the cards finish on flipping    
     setTimeout(() => {
     firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');  
-       
+    secondCard.classList.remove('flip'); 
+    wrongAs.play();
     resetBoard();
-  }, 1000)
+
+  }, 800)
 }
 
 
-function resetBoard() {  // to let our condition to work after each round.
+function resetBoard() {  // to let our condition to work after each round. (make you choose the same first card after you choose second one )
     hasFlippedCard = false;
     lockBoard = false;
     firstCard = null;
     secondCard = null;
+
 }
 
 (function shuffle() { // when you refresh the site or enter the level agin it will shuffle the cards
@@ -65,18 +109,3 @@ function resetBoard() {  // to let our condition to work after each round.
     });
 })(); //warp it inside a parenthesis and net next repair parenthesis to end of it that makes the function it will executed right after the player get in the or refresh it
 
-
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-
-overlays.forEach(overlay => {
-    overlay.addEventListener('click', () => {
-        overlay.classList.remove('visible');
-        var myAudio = document.createElement("audio");
-        myAudio.src = "sounds/StartGame.mp3";
-        myAudio.play();
-
-    });
-});
